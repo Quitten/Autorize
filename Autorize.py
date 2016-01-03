ec7db4bd7ce4a18b8782580704012b8fd5c5c998
Federico Dotta <dottafederico@gmail.com>
2016-01-03 19:50:29 +0100

    - Added Save/Restore functionality

--- Autorize.py
+++ Autorize.py
@@ -3,6 +3,7 @@ from burp import IBurpExtender
 from burp import IHttpListener
 from burp import IContextMenuFactory
 from burp import IMessageEditorController
+from burp import IHttpRequestResponse
 from javax.swing import JList
 from javax.swing import JTable
 from javax.swing import JFrame
@@ -44,6 +45,23 @@ from thread import start_new_thread
 from javax.swing import RowFilter
 from javax.swing.table import TableRowSorter
 from javax.swing import JScrollPane
+from java.awt import Font
+import csv
+import sys
+import base64
+
+# This code is necessary to maximize the csv field limit for the save and restore functionality
+maxInt = sys.maxsize
+decrement = True
+
+while decrement:
+
+    decrement = False
+    try:
+        csv.field_size_limit(maxInt)
+    except OverflowError:
+        maxInt = int(maxInt/10)
+        decrement = True
 
 class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController, AbstractTableModel, IContextMenuFactory):
 
@@ -146,34 +164,51 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
         ## init enforcement detector tab
         #
 
+        exportLabel = JLabel("Export:")
+        exportLabel.setBounds(10, 10, 100, 30)
+        labelFont = exportLabel.getFont()
+        boldFont = Font(labelFont.getFontName(), Font.BOLD, labelFont.getSize())
+        exportLabel.setFont(boldFont)
+
         exportLType = JLabel("File Type:")
-        exportLType.setBounds(10, 10, 100, 30)
-       
-        exportLES = JLabel("Enforcement Statuses:")
-        exportLES.setBounds(10, 50, 160, 30)
+        exportLType.setBounds(10, 50, 100, 30)
 
         exportFileTypes = ["HTML","CSV"]
         self.exportType = JComboBox(exportFileTypes)
-        self.exportType.setBounds(100, 10, 200, 30)
+        self.exportType.setBounds(100, 50, 200, 30)
 
         exportES = ["All Statuses", self._enfocementStatuses[0], self._enfocementStatuses[1], self._enfocementStatuses[2]]
         self.exportES = JComboBox(exportES)
-        self.exportES.setBounds(100, 50, 200, 30)
+        self.exportES.setBounds(100, 90, 200, 30)
 
         exportLES = JLabel("Statuses:")
-        exportLES.setBounds(10, 50, 100, 30)
+        exportLES.setBounds(10, 90, 100, 30)
 
         self.exportButton = JButton("Export",actionPerformed=self.export)
-        self.exportButton.setBounds(390, 25, 100, 30)
+        self.exportButton.setBounds(390, 65, 100, 30)
+
+        saveRestoreLabel = JLabel("Save / Restore:")
+        saveRestoreLabel.setBounds(10, 150, 100, 30)    
+        saveRestoreLabel.setFont(boldFont)    
+
+        self.saveStateButton = JButton("Save state",actionPerformed=self.saveStateAction)
+        self.saveStateButton.setBounds(10, 200, 100, 30)
+
+        self.restoreStateButton = JButton("Restore state",actionPerformed=self.restoreStateAction)
+        self.restoreStateButton.setBounds(390, 200, 100, 30)        
 
         self.exportPnl = JPanel()
         self.exportPnl.setLayout(None);
-        self.exportPnl.setBounds(0, 0, 1000, 1000);
+        self.exportPnl.setBounds(0, 0, 1000, 1000)
+        self.exportPnl.add(exportLabel)
         self.exportPnl.add(exportLType)
         self.exportPnl.add(self.exportType)
         self.exportPnl.add(exportLES)
         self.exportPnl.add(self.exportES)
         self.exportPnl.add(self.exportButton)
+        self.exportPnl.add(saveRestoreLabel)
+        self.exportPnl.add(self.saveStateButton)
+        self.exportPnl.add(self.restoreStateButton)
 
     def initEnforcementDetector(self):
         #
@@ -384,7 +419,7 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
         self.filtersTabs.addTab("Detector Unauthenticated", self.EDPnlUnauth)
         self.filtersTabs.addTab("Interception Filters", self.filtersPnl)
         self.filtersTabs.addTab("Table Filter", self.filterPnl)
-        self.filtersTabs.addTab("Export", self.exportPnl)
+        self.filtersTabs.addTab("Save/Restore", self.exportPnl)
 
         self.filtersTabs.setBounds(0, 280, 2000, 700)
 
@@ -565,10 +600,16 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
         else:
             self.exportToCSV()
 
+    def saveStateAction(self, event):
+        self.saveState()
+        
+    def restoreStateAction(self, event):
+        self.restoreState()            
+
     def exportToCSV(self):
         parentFrame = JFrame()
         fileChooser = JFileChooser()
-        fileChooser.setSelectedFile(File("AutorizeReprort.csv"));
+        fileChooser.setSelectedFile(File("AutorizeReport.csv"));
         fileChooser.setDialogTitle("Save Autorize Report")
         userSelection = fileChooser.showSaveDialog(parentFrame)
         if userSelection == JFileChooser.APPROVE_OPTION:
@@ -594,7 +635,7 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
     def exportToHTML(self):
         parentFrame = JFrame()
         fileChooser = JFileChooser()
-        fileChooser.setSelectedFile(File("AutorizeReprort.html"));
+        fileChooser.setSelectedFile(File("AutorizeReport.html"));
         fileChooser.setDialogTitle("Save Autorize Report")
         userSelection = fileChooser.showSaveDialog(parentFrame)
         if userSelection == JFileChooser.APPROVE_OPTION:
@@ -659,7 +700,121 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
         f.writelines(htmlContent)
         f.close()
 
+    # Save state
+    def saveState(self):
 
+        parentFrame = JFrame()
+        fileChooser = JFileChooser()
+        fileChooser.setDialogTitle("State output file")
+        userSelection = fileChooser.showSaveDialog(parentFrame)
+        
+        if userSelection == JFileChooser.APPROVE_OPTION:
+            
+            exportFile = fileChooser.getSelectedFile()
+
+            with open(exportFile.getAbsolutePath(), 'wb') as csvfile:
+
+                csvwriter = csv.writer(csvfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+                for i in range(0,self._log.size()):
+
+                    tempRequestResponseHost = self._log.get(i)._requestResponse.getHttpService().getHost()
+                    tempRequestResponsePort = self._log.get(i)._requestResponse.getHttpService().getPort()
+                    tempRequestResponseProtocol = self._log.get(i)._requestResponse.getHttpService().getProtocol()
+                    tempRequestResponseRequest = base64.b64encode(self._log.get(i)._requestResponse.getRequest())
+                    tempRequestResponseResponse = base64.b64encode(self._log.get(i)._requestResponse.getResponse())
+
+                    tempOriginalRequestResponseHost = self._log.get(i)._originalrequestResponse.getHttpService().getHost()
+                    tempOriginalRequestResponsePort = self._log.get(i)._originalrequestResponse.getHttpService().getPort()
+                    tempOriginalRequestResponseProtocol = self._log.get(i)._originalrequestResponse.getHttpService().getProtocol()
+                    tempOriginalRequestResponseRequest = base64.b64encode(self._log.get(i)._originalrequestResponse.getRequest())
+                    tempOriginalRequestResponseResponse   = base64.b64encode(self._log.get(i)._originalrequestResponse.getResponse())
+
+                    if self._log.get(i)._unauthorizedRequestResponse != None:
+                        tempUnauthorizedRequestResponseHost = self._log.get(i)._unauthorizedRequestResponse.getHttpService().getHost()
+                        tempUnauthorizedRequestResponsePort = self._log.get(i)._unauthorizedRequestResponse.getHttpService().getPort()
+                        tempUnauthorizedRequestResponseProtocol = self._log.get(i)._unauthorizedRequestResponse.getHttpService().getProtocol()
+                        tempUnauthorizedRequestResponseRequest = base64.b64encode(self._log.get(i)._unauthorizedRequestResponse.getRequest())
+                        tempUnauthorizedRequestResponseResponse = base64.b64encode(self._log.get(i)._unauthorizedRequestResponse.getResponse())
+                    else:
+                        tempUnauthorizedRequestResponseHost = None
+                        tempUnauthorizedRequestResponsePort = None
+                        tempUnauthorizedRequestResponseProtocol = None
+                        tempUnauthorizedRequestResponseRequest = None
+                        tempUnauthorizedRequestResponseResponse = None
+
+                    tempEnforcementStatus = self._log.get(i)._enfocementStatus
+                    tempEnforcementStatusUnauthorized  = self._log.get(i)._enfocementStatusUnauthorized           
+
+                    tempRow = [tempRequestResponseHost,tempRequestResponsePort,tempRequestResponseProtocol,tempRequestResponseRequest,tempRequestResponseResponse]
+                    tempRow.extend([tempOriginalRequestResponseHost,tempOriginalRequestResponsePort,tempOriginalRequestResponseProtocol,tempOriginalRequestResponseRequest,tempOriginalRequestResponseResponse])
+                    tempRow.extend([tempUnauthorizedRequestResponseHost,tempUnauthorizedRequestResponsePort,tempUnauthorizedRequestResponseProtocol,tempUnauthorizedRequestResponseRequest,tempUnauthorizedRequestResponseResponse])
+                    tempRow.extend([tempEnforcementStatus,tempEnforcementStatusUnauthorized])
+
+                    csvwriter.writerow(tempRow)
+
+    # Restore state
+    def restoreState(self):
+        parentFrame = JFrame()
+        fileChooser = JFileChooser()
+        fileChooser.setDialogTitle("State import file")
+        userSelection = fileChooser.showDialog(parentFrame,"Restore")
+        
+        if userSelection == JFileChooser.APPROVE_OPTION:
+            importFile = fileChooser.getSelectedFile()
+
+            with open(importFile.getAbsolutePath(), 'r') as csvfile:
+
+                csvreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+
+                for row in csvreader:
+
+                    tempRequestResponseHost = row[0]
+                    tempRequestResponsePort = row[1]
+                    tempRequestResponseProtocol = row[2]
+                    tempRequestResponseRequest = base64.b64decode(row[3])
+                    tempRequestResponseResponse = base64.b64decode(row[4])
+
+                    tempRequestResponseHttpService = self._helpers.buildHttpService(tempRequestResponseHost,int(tempRequestResponsePort),tempRequestResponseProtocol)
+                    tempRequestResponse = IHttpRequestResponseImplementation(tempRequestResponseHttpService,tempRequestResponseRequest,tempRequestResponseResponse)
+
+                    tempOriginalRequestResponseHost = row[5]
+                    tempOriginalRequestResponsePort = row[6]
+                    tempOriginalRequestResponseProtocol = row[7]
+                    tempOriginalRequestResponseRequest = base64.b64decode(row[8])
+                    tempOriginalRequestResponseResponse   = base64.b64decode(row[9])
+
+                    tempOriginalRequestResponseHttpService = self._helpers.buildHttpService(tempOriginalRequestResponseHost,int(tempOriginalRequestResponsePort),tempOriginalRequestResponseProtocol)
+                    tempOriginalRequestResponse = IHttpRequestResponseImplementation(tempOriginalRequestResponseHttpService,tempOriginalRequestResponseRequest,tempOriginalRequestResponseResponse)
+                    
+                    checkAuthentication = True
+                    if row[10] != '':
+                        tempUnauthorizedRequestResponseHost = row[10]
+                        tempUnauthorizedRequestResponsePort = row[11]
+                        tempUnauthorizedRequestResponseProtocol = row[12]
+                        tempUnauthorizedRequestResponseRequest = base64.b64decode(row[13])
+                        tempUnauthorizedRequestResponseResponse = base64.b64decode(row[14])
+                        tempUnauthorizedRequestResponseHttpService = self._helpers.buildHttpService(tempUnauthorizedRequestResponseHost,int(tempUnauthorizedRequestResponsePort),tempUnauthorizedRequestResponseProtocol)
+                        tempUnauthorizedRequestResponse = IHttpRequestResponseImplementation(tempUnauthorizedRequestResponseHttpService,tempUnauthorizedRequestResponseRequest,tempUnauthorizedRequestResponseResponse)
+                    else:
+                        checkAuthentication = False
+                        tempUnauthorizedRequestResponse = None                      
+
+                    tempEnforcementStatus = row[15]
+                    tempEnforcementStatusUnauthorized  = row[16]
+
+                    self._lock.acquire()
+        
+                    row = self._log.size()
+
+                    if checkAuthentication:
+                        self._log.add(LogEntry(self.currentRequestNumber,self._callbacks.saveBuffersToTempFiles(tempRequestResponse), self._helpers.analyzeRequest(tempRequestResponse).getMethod(), self._helpers.analyzeRequest(tempRequestResponse).getUrl(), self._callbacks.saveBuffersToTempFiles(tempOriginalRequestResponse),tempEnforcementStatus,self._callbacks.saveBuffersToTempFiles(tempUnauthorizedRequestResponse),tempEnforcementStatusUnauthorized))
+                    else:
+                        self._log.add(LogEntry(self.currentRequestNumber,self._callbacks.saveBuffersToTempFiles(tempRequestResponse), self._helpers.analyzeRequest(tempRequestResponse).getMethod(), self._helpers.analyzeRequest(tempRequestResponse).getUrl(), self._callbacks.saveBuffersToTempFiles(tempOriginalRequestResponse),tempEnforcementStatus,None,tempEnforcementStatusUnauthorized))
+                    
+                    self.fireTableRowsInserted(row, row)
+                    self.currentRequestNumber = self.currentRequestNumber + 1
+                    self._lock.release()
 
 
     #
@@ -1130,3 +1285,53 @@ class tableFilter(RowFilter):
             return True            
         else:
             return False
+
+class IHttpRequestResponseImplementation(IHttpRequestResponse):
+
+    def __init__(self, service, req, res):
+        
+        self._httpService = service
+        self._request = req
+        self._response = res
+        self._comment = None
+        self._highlight = None
+
+    def getComment(self):
+
+        return self._comment
+
+    def getHighlight(self):
+
+        return self._highlight
+
+    def getHttpService(self):
+
+        return self._httpService
+
+    def getRequest(self):
+
+        return self._request
+
+    def getResponse(self):
+
+        return self._response
+
+    def setComment(self,c):
+
+        self._comment = c
+
+    def setHighlight(self,h):
+
+        self._highlight = h
+
+    def setHttpService(self,service):
+
+        self._httpService = service
+
+    def setRequest(self,req):
+
+        self._request = req
+
+    def setResponse(self,res):
+
+        self._response = res
